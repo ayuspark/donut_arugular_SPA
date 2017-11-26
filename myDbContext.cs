c0c290921951518e93ce23d03b42d215d189b958
ayuspark <lola.luyaowang@gmail.com>
2017-11-26 00:24:55 -0800

    add dbContext

---
+++ myDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace donut_arugular_SPA
+{
+    public class myDbContext : DbContext 
+    {
+        public myDbContext(string connectionString)
+        {
+            
+        }
+    }
+}
