cfe94b2d5fbbf433e0e3b5a43f4c7226c36e18e4
ayuspark <lola.luyaowang@gmail.com>
2017-11-26 14:14:30 -0800

    migrated

--- Models/Model.cs
+++ Models/Model.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace donut_arugular_SPA.Models
@@ -5,9 +6,11 @@ namespace donut_arugular_SPA.Models
     public class Model
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public Make Make { get; set; }
-        
+
         [ForeignKey("Make")]
         public int MakeId { get; set; }
     }
--- myDbContext.cs
+++ myDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using donut_arugular_SPA.Models;
 
 namespace donut_arugular_SPA
 {
@@ -6,7 +7,9 @@ namespace donut_arugular_SPA
     {
         public myDbContext(DbContextOptions<myDbContext> options) : base(options)
         {
-            
         }
+
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Model> Models { get; set; }
     }
 }
