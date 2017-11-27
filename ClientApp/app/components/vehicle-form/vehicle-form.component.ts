f414f98c023fd2f8d2e80f469fa23f093ad0c0b5
ayuspark <lola.luyaowang@gmail.com>
2017-11-27 15:36:45 -0800

    service gets Make[], component subscribed hence get Make[], populate models according to selectedMake

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MakeService } from '../../services/make.service'
 
 @Component({
   selector: 'app-vehicle-form',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehicle-form.component.css']
 })
 export class VehicleFormComponent implements OnInit {
+  makes: any[];
+  models: any[];
+  vehicle:any = {};
 
-  constructor() { }
+  constructor(private _makeService: MakeService) { }
 
   ngOnInit() {
+    this._makeService.getMakes().subscribe(m => {
+      this.makes = m;
+      console.log("Make: ", this.makes);
+    });
+  }
+
+  onMakeChange() {
+    console.log("VEHICLE MAKE CHOSEN: ", this.vehicle);
+    var selectedMake = this.makes.find(m => m.id == this.vehicle.makeId);
+    console.log("SELECTED MAKE: ", selectedMake);
+    this.models = selectedMake? selectedMake.models:[];
+    console.log("POPULATE MODELS: ", this.models);
   }
 
 }
