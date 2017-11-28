6912a9ee3963efdfde4fad50ecfa4ca484d523d9
ayuspark <lola.luyaowang@gmail.com>
2017-11-27 16:01:13 -0800

    added featureService, about to change DB

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MakeService } from '../../services/make.service'
+import { FeatureService } from '../../services/feature.service';
 
 @Component({
   selector: 'app-vehicle-form',
@@ -10,14 +11,17 @@ export class VehicleFormComponent implements OnInit {
   makes: any[];
   models: any[];
   vehicle:any = {};
+  features: any[];
 
-  constructor(private _makeService: MakeService) { }
+  constructor(private _makeService: MakeService, private _featureService: FeatureService) { }
 
   ngOnInit() {
     this._makeService.getMakes().subscribe(m => {
       this.makes = m;
       console.log("Make: ", this.makes);
     });
+
+    this._featureService.getFeatures().subscribe(f => {this.features = f})
   }
 
   onMakeChange() {
---
+++ Models/Feature.cs
@@ -0,0 +1,8 @@
+namespace donut_arugular_SPA.Models
+{
+    public class Feature
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
