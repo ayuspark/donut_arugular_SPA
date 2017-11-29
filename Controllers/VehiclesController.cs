f9ae49e9791546b1ff1b1573c5a5b9243b692505
ayuspark <lola.luyaowang@gmail.com>
2017-11-28 18:35:43 -0800

    finished VehicleController

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -55,7 +55,11 @@ namespace donut_arugular_SPA.Controllers
 
             var vehicle = await _context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
             
-            
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
             _mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
             vehicle.LastUpdate = DateTime.Now;
 
@@ -63,6 +67,35 @@ namespace donut_arugular_SPA.Controllers
 
             var result = _mapper.Map<Vehicle, VehicleResource>(vehicle);
             return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicleAsync(int id)
+        {
+            var vehicle = await _context.Vehicles.FindAsync(id);
+
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+            _context.Vehicles.Remove(vehicle);
+            await _context.SaveChangesAsync();
+
+            return Ok(id);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVehicle(int id)
+        {
+            var vehicle = await _context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+            
+            var vehicleResource = _mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+            return Ok(vehicleResource);
         }
     }
 }
