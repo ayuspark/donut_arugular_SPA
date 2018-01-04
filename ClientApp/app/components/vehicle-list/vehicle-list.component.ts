632455b5f72f01e0967675abbfe5421b2483f04c
ayuspark <lola.luyaowang@gmail.com>
2018-01-04 23:29:07 +0800

    added client side filter

--- ClientApp/app/components/vehicle-list/vehicle-list.component.ts
+++ ClientApp/app/components/vehicle-list/vehicle-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Vehicle } from '../../models/Vehicle';
+import { Vehicle, KeyValuePair } from '../../models/Vehicle';
 import { VehicleService } from '../../services/vehicle.service';
 
 @Component({
@@ -9,12 +9,32 @@ import { VehicleService } from '../../services/vehicle.service';
 })
 export class VehicleListComponent implements OnInit {
   vehicles: Vehicle[];
+  allVehicles: Vehicle[];
+  makes: KeyValuePair[];
+  filter: any = {};
 
   constructor(private _vehicleService: VehicleService) {}
 
   ngOnInit() {
-    this._vehicleService.allVehicles().subscribe(vehicles => {this.vehicles = vehicles;
-      console.log('~~~~~', this.vehicles);});
+    this._vehicleService.allVehicles().subscribe(vehicles =>
+      {
+        this.vehicles = vehicles;
+        this.allVehicles = vehicles;
+      });
+
+    this._vehicleService.getMakes().subscribe(makes => this.makes = makes);
+  }
+
+  onFilterChange(){
+    var vehicles = this.allVehicles;
+    if(this.filter.makeId) {
+      vehicles = vehicles.filter(v => v.make.id == this.filter.makeId);
+    }
+    this.vehicles = vehicles;
+  }
+
+  onResetClick(){
+    this.vehicles = this.allVehicles;
   }
 
 }
